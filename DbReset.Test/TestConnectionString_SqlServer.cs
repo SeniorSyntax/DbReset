0f1a307ed7051ed7d1574bcd172e6b661bc59a27
Mathias Stenbom <mathias.stenbom@calabrio.com>
2023-07-14 15:14:26 +0200

    connection string for pipe

--- DbReset.Test/TestConnectionString_SqlServer.cs
+++ DbReset.Test/TestConnectionString_SqlServer.cs
@@ -2,7 +2,11 @@ namespace DbReset.Test;
 
 public static class TestConnectionString
 {
+#if DEBUG
 	public const string ConnectionString =
 		"Data Source=.;Integrated Security=True;Initial Catalog=DbReset.Test;";
-
+#else
+	public const string ConnectionString =
+		"Data Source=.\SQLEXPRESS;Integrated Security=True;Initial Catalog=DbReset.Test;";
+#endif
 }
