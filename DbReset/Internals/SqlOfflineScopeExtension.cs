2671b7807dd20bca3ee043d2c35929779c0a53b7
Roger Kratz <roger.kratz@gmail.com>
2026-03-04 16:08:39 +0100

    Revert "lets kill all spids properly before going offline"
    This reverts commit 6f492a45360328e888f09e8f9762807144e2e1bc.

--- DbReset/Internals/SqlOfflineScopeExtension.cs
+++ DbReset/Internals/SqlOfflineScopeExtension.cs
@@ -7,23 +7,11 @@ internal static class SqlOfflineScopeExtension
 {
 	public static IDisposable OfflineScope(this ICacheContext context)
 	{
-		var databaseName = context.DatabaseName();
-		var killSql = $@"
-DECLARE @kills nvarchar(max) = N'';
-
-SELECT @kills = @kills + N'KILL ' + CAST(session_id AS nvarchar(10)) + N';'
-FROM sys.dm_exec_sessions
-WHERE is_user_process = 1
-  AND session_id <> @@SPID
-  AND database_id = DB_ID(N'{databaseName}');
-
-IF (@kills <> N'')
-    EXEC sp_executesql @kills;
-";
-		context.MasterConnector().Execute(killSql);
+		SqlConnection.ClearAllPools();
 		context.MasterConnector().Execute($"ALTER DATABASE [{context.DatabaseName()}] SET OFFLINE WITH ROLLBACK IMMEDIATE");
 		return new GenericDisposable(() =>
 		{
+			//
 			context.MasterConnector().Execute($"ALTER DATABASE [{context.DatabaseName()}] SET ONLINE");
 		});
 	}
