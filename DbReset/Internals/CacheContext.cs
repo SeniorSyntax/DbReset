cc63db695d24667e2e4c83dfe3050daa6bd2c050
Roger Kratz <roger.kratz@gmail.com>
2026-04-08 16:40:25 +0200

    fix for postgres, green build now hopefully

--- DbReset/Internals/CacheContext.cs
+++ DbReset/Internals/CacheContext.cs
@@ -25,14 +25,22 @@ internal class CacheContext : ICacheContext
 
 	public void LogInfo(string message) => Output?.Info(message);
 	
-	private static bool? _dbRunsOnWindows;
-	public string TempFolder()
-	{
-		_dbRunsOnWindows ??= ((ICacheContext)this).MasterConnector()
-			.ExecuteScalar<string>("select host_platform from sys.dm_os_host_info;") == "Windows";
-		
-		return _dbRunsOnWindows.Value ? 
+	public string TempFolder() =>
+		dbRunsOnWindows() ? 
 			DatabaseTempFolders.ForWindows : 
 			DatabaseTempFolders.ForLinux;
+
+	private static bool? _dbRunsOnWindows;
+	private bool dbRunsOnWindows()
+	{
+		if (!_dbRunsOnWindows.HasValue)
+		{
+			var connector = ((ICacheContext)this).MasterConnector();
+			_dbRunsOnWindows = ConnectionString.PickFunc(
+				() => connector.ExecuteScalar<string>("select host_platform from sys.dm_os_host_info;") == "Windows",
+				() => !connector.ExecuteScalar<string>("SELECT version();").Contains("linux")
+			);
+		}
+		return _dbRunsOnWindows.Value;
 	}
 }
